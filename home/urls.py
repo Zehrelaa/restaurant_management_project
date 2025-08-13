3e9fd956d8cceb1b1fe95ef244c808b032401492
Samarth Gupta <153947799+Zehrelaa@users.noreply.github.com>
2025-08-13 17:08:11 +0530

    28871

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path
-from .views import *
+from django.urls import path 
+from .import views 
 
-urlpatterns = [
-    
-]
+urlpatterns = [path('about/' , views.about_view , name='about ') , ]
