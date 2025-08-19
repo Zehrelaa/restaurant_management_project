3b1990ac7d42283d73866f227ad62cae21b17ada
Samarth Gupta <153947799+Zehrelaa@users.noreply.github.com>
2025-08-19 23:24:37 +0530

    28869

--- home/views.py
+++ home/views.py
@@ -2,4 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def about_view(request):
-    return render (request , 'about.html')
+    return render (request , 'about.html') 
+
+def home(request):
+    return HttpResponse("""
+     <html>
+     <head>
+     <title> Homepage </title>
+     </head>
+     <body style = "font-family: Arial , sans-serif ; background-color : #f9f9f9 ; text-align : center ; padding : 50px; " >
+     <h1 style="color: #4CAF50 ; font-size : 48 px ; margin-bottom: 20px; ">
+     Welcome to My Homepage 
+     <h1>
+     <p style+"font-size : 20px; color:#555; ">
+     This is the sample homepage style in inline css.
+     </p>
+     <a href="/about" style="display :inline-block ; margin-top : 20px ; padding 10px 20px ; background-color : #4CAF50 ; color:white ; 
+     tect-decoration: none ; border-radius: 5px " >
+     Learn More
+     </a>
+     </body>
+     </html>
+
+
+    """)
